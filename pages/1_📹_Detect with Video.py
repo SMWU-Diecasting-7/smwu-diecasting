fc1143a770939230bb27c2c498bf1d99a6156828
becherryu <81617058+becherryu@users.noreply.github.com>
2024-11-25 18:54:40 +0900

    feat: make sidebar tabs

---
+++ pages/1_📹_Detect with Video.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# 페이지 설정
+st.set_page_config(
+    page_title="Detect with Video",
+    page_icon="📹",
+)
+
---
+++ pages/2_📸_Detect with Image.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Detect with Image",
+    page_icon="📸",
+)
+
